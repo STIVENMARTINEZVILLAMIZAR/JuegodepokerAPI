30567c952a69ff477dada18684903f01f062fdd7
stiven <stiven@gmail.com>
2025-07-18 11:30:21 -0500

    FEAT: :zap: ADD FILES DIA1

---
+++ DIA1/scripts/scripts.js
@@ -0,0 +1,8 @@
+//##############################
+//######### DIA1###############
+//##############################
+
+// PRIMER COMAND!!!
+
+console.log("hola mundo!");
+// DESARROLLADO POR STIVEN MARTINEZ VILLMIAZRA: Cc 1096064595
