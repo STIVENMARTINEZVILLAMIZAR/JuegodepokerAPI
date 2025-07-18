bd7fcbc913f6b1050d7d25d30beb33c307146f71
stiven <stiven@gmail.com>
2025-07-18 11:34:18 -0500

    FEAT: :zap: UPDATE CODE

--- DIA1/scripts/scripts.js
+++ DIA1/scripts/scripts.js
@@ -5,4 +5,7 @@
 // PRIMER COMAND!!!
 
 console.log("hola mundo!");
+variable1= 3.14;
+console.log(variable1);
+console.log(typeof variable1);
 // DESARROLLADO POR STIVEN MARTINEZ VILLMIAZRA: Cc 1096064595
