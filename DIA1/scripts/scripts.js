d85d6b3859dea80ab381ebda26f54f8775f160ee
stiven <stiven@gmail.com>
2025-07-18 11:44:28 -0500

    FEAT: :zap: UPDATE CODE

--- DIA1/scripts/scripts.js
+++ DIA1/scripts/scripts.js
@@ -5,7 +5,30 @@
 // PRIMER COMAND!!!
 
 console.log("hola mundo!");
+//Number
 variable1= 3.14;
 console.log(variable1);
 console.log(typeof variable1);
+//Concatenacion
+palabra1= "campus";
+palabra2= "lands";
+palabra3= palabra1+palabra2;
+console.log(palabra3);
+
+nombrePersona="Luis angel";
+plataActual=22000;
+console.log("la persona llamada", nombrePersona,"tiene en su cuenta bancaria", plataActual,"pesos colombianos!");
+
+
+
+
+
+
+
+
+
+
+
+
+
 // DESARROLLADO POR STIVEN MARTINEZ VILLMIAZRA: Cc 1096064595
