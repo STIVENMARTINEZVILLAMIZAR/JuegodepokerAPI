fa288621517fe7096e10b7afd9e4b20e3b1ab269
zzz <zzz@gmail.com>
2025-07-24 11:49:54 -0500

    FEAT: :zap: UPDATE CODE

--- DIA4/script/paginascript.js
+++ DIA4/script/paginascript.js
@@ -14,10 +14,12 @@ while (boleanito ==true) {
 Bienvenido al simulador de gastos de la cafetería
 ========================================
      1. Menu de hamburguesas
-     2. dame los ingredientes de la hamburguesa
-     3. dame la categoria de la hamburguesa
-     4. deseas actualizar tu pedido?
-     5. salir del simulador
+     2. Añadir
+     3. Listar
+     4. Eliminar
+     5. Actualizar
+     6. Consultar
+     7. Salir
 
      
      seleccione una opcion:
@@ -25,13 +27,62 @@ Bienvenido al simulador de gastos de la cafetería
 `)
 
 console.log ("Bienvenido al simulador de gastos de la cafetería")
-console.log ("1.Menu de hamburguesas")
-console.log ("2.dame los ingredientes de la hamburguesa")
-console.log ("3. dame la categoria de la hamburguesa")
-console.log ("4. deseas actualizar tu pedido?")
-console.log ("5. salir del simulador")
+console.log ("1. Menu de hamburguesas")
+console.log ("2. Añadir")
+console.log ("3. Listar")
+console.log ("4. Eliminar")
+console.log ("5. Actualizar")
+console.log ("6. Consultar")
+console.log ("7. Salir")
 
-/*
+
+let seleccionar= prompt ("seleccione una opcion:")
+
+   let consultar = prompt (`
+
+ 1. Encontrar todos los ingredientes con stock menor a 400.
+ 2. Encontrar todas las hamburguesas de la categoría “Vegetariana”.
+ 3. Encontrar todos los chefs que se especializan en “Carnes”.
+ 4. Aumentar en 1.5 el precio de todos los ingredientes.
+ 5. Encontrar todas las hamburguesas preparadas por “ChefB”.
+ 6. Encontrar el nombre y la descripción de todas las categorías.
+ 7. Eliminar todos los ingredientes que tengan un stock de 0.
+ 8. Agregar un nuevo ingrediente a la hamburguesa “Clásica”.
+ 9. Encontrar todas las hamburguesas que contienen “Pan integral” como ingrediente.
+ 10. Cambiar la especialidad del “ChefC” a “Cocina Internacional”.
+ 11. Encontrar el ingrediente más caro.
+ 12. Encontrar las hamburguesas que no contienen “Queso cheddar” como ingrediente.
+ 13. Incrementar el stock de “Pan” en 100 unidades.
+ 14. Encontrar todos los ingredientes que tienen una descripción que contiene la palabra “clásico”.
+ 15. Listar las hamburguesas cuyo precio es menor o igual a $9.
+ 16. Contar cuántos chefs hay en la base de datos.
+ 17. Encontrar todas las categorías que contienen la palabra “gourmet” en su descripción.
+ 18. Eliminar las hamburguesas que contienen menos de 5 ingredientes.
+ 19. Agregar un nuevo chef a la colección con una especialidad en “Cocina Asiática”.
+ 20. Listar las hamburguesas en orden ascendente según su precio.
+ 21. Encontrar todos los ingredientes cuyo precio sea entre $2 y $5.
+ 22. Actualizar la descripción del “Pan” a “Pan fresco y crujiente”.
+ 23. Encontrar todas las hamburguesas que contienen “Tomate” o “Lechuga” como ingredientes.
+ 24. Listar todos los chefs excepto “ChefA”.
+ 25. Incrementar en $2 el precio de todas las hamburguesas de la categoría “Gourmet”.
+ 26. Listar todos los ingredientes en orden alfabético.
+ 27. Encontrar la hamburguesa más cara.
+ 28. Agregar “Pepinillos” a todas las hamburguesas de la categoría “Clásica”.
+ 29. Eliminar todos los chefs que tienen una especialidad en “Cocina Vegetariana”.
+ 30. Encontrar todas las hamburguesas que contienen exactamente 7 ingredientes.
+ 31. Encontrar la hamburguesa más cara que fue preparada por un chef especializado en “Gourmet”.
+ 32. Listar todos los ingredientes junto con el número de hamburguesas que los contienen.
+ 33. Listar los chefs junto con el número de hamburguesas que han preparado.
+ 34. Encuentra la categoría con la mayor cantidad de hamburguesas.
+ 35. Listar todos los chefs y el costo total de ingredientes de todas las hamburguesas que han preparado.
+ 36. Encontrar todos los ingredientes que no están en ninguna hamburguesa.
+ 37. Listar todas las hamburguesas con su descripción de categoría.
+ 38. Encuentra el chef que ha preparado hamburguesas con el mayor número de ingredientes en total.
+ 39. Encontrar el precio promedio de las hamburguesas en cada categoría.
+ 40. Listar los chefs y la hamburguesa más cara que han preparado.
+
+`)
+
 console.log("1. Encontrar todos los ingredientes con stock menor a 400.")
 console.log("2. Encontrar todas las hamburguesas de la categoría “Vegetariana”.")
 console.log("3. Encontrar todos los chefs que se especializan en “Carnes”.")
@@ -72,7 +123,6 @@ console.log("37. Listar todas las hamburguesas con su descripción de categoría
 console.log("38. Encuentra el chef que ha preparado hamburguesas con el mayor número de ingredientes en total.")
 console.log("39. Encontrar el precio promedio de las hamburguesas en cada categoría.")
 console.log("40. Listar los chefs y la hamburguesa más cara que han preparado.")
-*/
 
 let opcion= prompt ("seleccione una opcion:")
 
