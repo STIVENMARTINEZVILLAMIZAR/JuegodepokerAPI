973b718e6f435b521b736b03a8f24313389473b9
zzz <zzz@gmail.com>
2025-07-28 13:02:34 -0500

    FEAT: :zap: UPDATE CODE

---
+++ DIA6/script/script.js
@@ -0,0 +1,26 @@
+
+let personajes = [];
+let boleanito = true;
+while (boleanito ==true) {
+    let menu = prompt (`
+======================================== 
+Bienvenido al simulador de rick and morty
+========================================
+     1. consultar personajes
+     2. Salir
+
+     
+     seleccione una opcion:
+
+`)
+
+alert.console.log(menu);
+
+
+
+
+let opcion= prompt ("seleccione una opcion:")
+
+
+
+}
