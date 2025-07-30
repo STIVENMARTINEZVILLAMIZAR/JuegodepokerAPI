96de810b9dd2bd2648bcaaf4cfa36d2923bcecf0
zzz <zzz@gmail.com>
2025-07-30 12:32:55 -0500

    FEAT: :zap: UPDATE CODE API DIA9

--- DIA9/XML/script.js
+++ DIA9/XML/script.js
@@ -6,6 +6,7 @@
 
 
 function buscarPersonaje() {
+    document.getElementById("buscarBtn").addEventListener("click", buscar);
     //const nombreUsar = prompt("Ingrese el nombre del personaje");
     const nombreUsar= document.getElementById("nombreInput").value;
     console.log(nombreUsar);
@@ -17,27 +18,53 @@ function buscarPersonaje() {
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 3) {
 
-            //alert("Cargando...");
+            console.log("Cargando...");
         }
         else if (xhr.readyState === 4 && xhr.status === 200) {
 
             try {
+
+             //   <div class="card" >
+               //  <img src="./media/rickabeja.jpeg" alt="">
+               //  <h3>Rick</h3>
+               //  <br>
+               //   <p><strong>Estado: Vivo</strong></p>
+               //   <br>
+               //   <p><strong>Especie: Humana</strong></p>
+               //  
+            // </div>
+
                 const daticos = JSON.parse(xhr.responseText);
                 //alert("The caracter is: Name:" + daticos['results'][0]['name'] + "Status:" + daticos['results'][0]['status'] + "Especie: " + daticos['results'][0]['species']);
-            }
-            catch (error) {
-                console.log(err.message);
+                
 
-            }
 
-        }
 
-    };
+
+                try {
+                    if (daticos.results && daticos.results.length > 0) {
+                        for(let i = 0; i < daticos.results.length; i++) {
+                            let division = document.createElement("div");
+                            division.className = "card";
+                            const personaje = daticos.results[i];
+                            console.log(daticos['results'][i]['name']);
+
+                        }
+                    }
+                } catch (error) {
+                    console.log(error.message);
+                }
+
+    } catch (error) {
+        console.error("Error:", error);
+    }
 
     xhr.send();
 
 }
 
-
-
+    }
+}
+    
 buscarPersonaje();
+    
