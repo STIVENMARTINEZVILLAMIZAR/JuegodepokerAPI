b74160b3fd89841e26bc732eedcae719772fd749
unknown <stiven250904@gmail.com>
2025-07-29 22:27:13 -0500

    FEAT: :zap: UPDATE CODE API

--- DIA8/Javascript - Practica Consumo/XML/script.js
+++ DIA8/Javascript - Practica Consumo/XML/script.js
@@ -5,7 +5,7 @@ function buscarPersonaje() {
     // const nombreUsar = prompt("Ingrese el nombre a buscar");
     const xhr = new XMLHttpRequest();
     //let nombreUsar= "mee"
-    const url = `(https://swapi.py4e.com/api/people/`;
+    const url = `https://swapi.py4e.com/api/people/`;
     console.log(url);
     xhr.open("GET", url, true);
     xhr.onreadystatechange = function () {
