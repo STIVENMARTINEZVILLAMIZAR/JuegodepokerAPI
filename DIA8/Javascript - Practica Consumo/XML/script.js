95f384cb1772dc4d7a6819302d48a3a815bbd5d4
unknown <stiven250904@gmail.com>
2025-07-29 22:06:56 -0500

    FEAT: :zap: UPDATE CODE API

--- DIA8/Javascript - Practica Consumo/XML/script.js
+++ DIA8/Javascript - Practica Consumo/XML/script.js
@@ -2,36 +2,20 @@
 
 
 function buscarPersonaje() {
-    const nombreUsar = prompt("Ingrese el nombre a buscar");
+    // const nombreUsar = prompt("Ingrese el nombre a buscar");
     const xhr = new XMLHttpRequest();
     //let nombreUsar= "mee"
-    const url = `(https://swapi.py4e.com/api/people?name=${nombreUsar}`;
+    const url = `(https://swapi.py4e.com/api/people/`;
     console.log(url);
     xhr.open("GET", url, true);
     xhr.onreadystatechange = function () {
-        if (xhr.readyState === 3) {
 
-            alert("Cargando...");
-        }
-        else if (xhr.readyState === 4 && xhr.status === 200) {
+        try{
 
-            try {
-                const daticos = JSON.parse(xhr.responseText);
-                alert("El personaje es: /n Nombre:" + daticos['results'][0]['name'] + "/n Altura: " + daticos ["results"][0]["height"]+ "/n Masa: " + daticos ["results"][0]["mass"]+ "/n color de cabello:" + daticos["results"][0]["hair_color"]+ "/n color de skin:" + daticos ["results"][0]["skin_color"] + "/n color de ojo:" +daticos ["results"][0]["eye_color"] );
-            }
-            catch (error) {
-                console.log(err.message);
+             if (xhr.readyState === 4 && xhr.status === 200) {
+                const data = JSON.parse(xhr.responseText);
+                console.log(data.results);
+                const mainBox = document.getElementById('main-box');
+                mainBox.innerHTML = "";
 
-            }
-
-        }
-
-    };
-
-    xhr.send();
-
-}
-
-
-
-buscarPersonaje();
+  
